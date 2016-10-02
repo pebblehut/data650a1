ff1bc79ce64e307e7d0e2c809d7c35e11bfa7fd9
David Stonehouse <d_stonehouse@yahoo.com>
2016-10-02 08:28:22 -0400

    comments for tf-idf subset

--- my_tf_idf.R
+++ my_tf_idf.R
@@ -33,15 +33,22 @@ m.tf.idf.total <- colSums(m.tf.idf)
 m.tf.idf.total[1:10]
 m[ ,"leesvill"]
 
+
+# subset just the terms with the highest tf-idf value ---------------------
+
+# First, add the tf-idf totals as an additional row
 m.tf.idf.tmp <- m.tf.idf
 m.tf.idf.tmp <- rbind(m.tf.idf.tmp, m.tf.idf.total)
-
 m.tf.idf.tmp[ ,1:10]
-m.tf.idf.transpose <- t(m.tf.idf.tmp)
 
+# Now prepare for subset command - transpose terms to be rows
+m.tf.idf.transpose <- t(m.tf.idf.tmp)
 m.tf.idf.transpose[1:9,]
 dim(m.tf.idf.transpose)
+
+# subset on the tf-idf-total - experiment on the threshold  
+# but drop tf-idf-total by only getting columns 1:ncorpus
 m.tf.idf.transpose2 <- subset(m.tf.idf.transpose, m.tf.idf.total > 30, c(1:ncorpus))
 dim(m.tf.idf.transpose2)
 m.tf.idf.transpose2
-m.tf.idfe <- t(m.tf.idf)
+# m.tf.idfe <- t(m.tf.idf)
